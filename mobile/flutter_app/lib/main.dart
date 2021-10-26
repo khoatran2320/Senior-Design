2cb0d9e3872eac9b93eb643bcd2604b55f4c7d17
myliang47 <mellaliang47@gmail.com>
2021-10-26 15:53:13 -0400

    Revert "added auth logic for app"
    This reverts commit fae0f4920bb89e57e51ae9d4bcb41bdf7dd5b7e2.

--- mobile/flutter_app/lib/main.dart
+++ mobile/flutter_app/lib/main.dart
@@ -1,57 +1,22 @@
-///********** Module Packages Import **********///
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
+import "splash.dart";
+import "./colors.dart";
 
-import './utils/authentication_service.dart';
-
-///********** UI Import **********///
-import "./pages/splash.dart";
-import "./utils/colors.dart";
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create: (_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        StreamProvider(
-          create: (context) =>
-              context.read<AuthenticationService>().authStateChanges,
-          initialData: null,
-        )
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch:
-              MaterialColor(ColorPallete.cPrimaryAsHex, ColorPallete.materialPrimary),
-        ),
-        home: const Splash(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xff0892A5, materialColorGenerator(8, 146, 165)),
       ),
+      home: const Splash(),
     );
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final firebaseUser = context.watch<User>();
-
-    if (firebaseUser != null) {
-      return Text('Logged In');
-    }
-    return Text('Sign In!');
   }
 }
