1c56069afa54b5c9c8e62a1ca93845f43599ee9d
myliang47 <mellaliang47@gmail.com>
2022-01-30 15:04:03 -0500

    delete flutter toast

--- mobile/flutter_app/lib/widgets/deliveries_screen/add_package_form.dart
+++ mobile/flutter_app/lib/widgets/deliveries_screen/add_package_form.dart
@@ -2,7 +2,6 @@
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 class AddPackageForm extends StatefulWidget {
