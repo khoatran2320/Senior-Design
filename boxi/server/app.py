f942542ce05fb5c11312919e8acb0b461ed6413e
Kenneth <kenc@bu.edu>
2022-02-01 14:13:46 -0500

    testing git

--- boxi/server/app.py
+++ boxi/server/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
 	return 'Hello World\n'
 
+@app.route('/barcode')
+def barcode():
+    return True
+
+@app.route('/lock')
+def lock():
+    return True
+
+@app.route('/weight')
+def weight():
+    return True
+
+@app.route('/vibration')
+def vibration():
+    return True
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='4321')
