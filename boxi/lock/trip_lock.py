d271be155175aa80a22a5558ff0d80bcb5ba07a4
Kenneth <kenc@bu.edu>
2022-02-02 02:53:57 -0500

    lock status, doenst work yet

--- boxi/lock/trip_lock.py
+++ boxi/lock/trip_lock.py
@@ -6,9 +6,12 @@ GPIO.setwarnings(False)
 # setting gpio pin 36 as out pin
 
 lock_trip = 36
+lock_status = 33
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(lock_trip, GPIO.OUT)
+
+GPIO.setup(lock_status, GPIO.IN)
 
 # trip func: outputs on gpio pin 36 for 1 millisecond
 def trip():
@@ -16,5 +19,13 @@ def trip():
     time.sleep(0.1)
     GPIO.output(lock_trip, GPIO.LOW)
 
+def is_trip():
+	locked = GPIO.input(lock_status)
+	if(locked):
+		print('locked')
+	else:
+		print('not locked')
+
 if __name__ == "__main__":
-	trip()
+	#trip()
+	is_trip()
