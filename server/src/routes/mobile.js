8a369c8a3d0a6b036c900b9e4d4a2f6ca693b1e0
Khoa Tran <kttran2320@gmail.com>
2022-02-27 14:23:01 -0500

    added mobile route for alarm box

--- server/src/routes/mobile.js
+++ server/src/routes/mobile.js
@@ -72,4 +72,63 @@ router.post('/unlock-box', async (req, res) => {
 	return;
 });
 
+router.post('/signal-alarm', async (req, res) => {
+	// Update a package to received for given Boxi ID
+	const { userId, boxiId } = req.body;
+
+	if (!userId) {
+		res.status(400).send('Requires a user ID!');
+		return;
+	}
+	if (!boxiId) {
+		res.status(400).send('Requires a box ID!');
+		return;
+	}
+
+	// check if user exist
+	try {
+		await auth.getAuth().getUser(userId);
+	} catch (err) {
+		//unable to find user
+		if (err['errorInfo']['code'] == 'auth/user-not-found') {
+			res.status(400).send('User does not exist');
+			return;
+		}
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+
+	// check if user owns this box
+	let userOwnsBox = false;
+	try {
+		const sshot = await db.collection(PACKAGE_COLLECTION).doc(userId).collection('boxes').get();
+		sshot.forEach((doc) => {
+			if (doc.id == boxiId) {
+				userOwnsBox = true;
+			}
+		});
+	} catch (err) {
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+
+	if (!userOwnsBox) {
+		res.status(403).send('User does not have access to this box!');
+		return;
+	}
+
+    try{
+        axios.post('http://10.192.4.148:4321/alarm', {
+            'userId': userId,
+            'boxiId': boxiId
+          });
+    }
+    catch(err){
+        res.status(400).send('Unable to open box!');
+		return;
+    }
+	res.status(200).send('Success!');
+	return;
+});
+
 module.exports = router;
