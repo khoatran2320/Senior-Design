d18563c1eb2017108b5987ad564dade7475e7866
Khoa Tran <kttran2320@gmail.com>
2022-02-27 15:44:28 -0500

    added automatic ip posting for Pi

---
+++ boxi/server/get_ip_addr.py
@@ -0,0 +1,11 @@
+import socket
+
+def get_ip_addr():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_addr = s.getsockname()[0]
+    s.close()
+    return ip_addr
+
+if __name__ == "__main__":
+    print(get_ip_addr())
