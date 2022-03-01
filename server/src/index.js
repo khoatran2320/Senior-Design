68a7d206724ffacd533d5c153528e883c158de3f
Khoa Tran <kttran2320@gmail.com>
2022-02-28 23:06:27 -0500

    get ip addr

--- server/src/index.js
+++ server/src/index.js
@@ -26,6 +26,11 @@ app.use('/package', packageRouter);
 app.use('/boxi', boxiRouter);
 app.use('/mobile', mobileRouter);
 
+function get_ip_addr(){
+    return Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])[0]
+}
+
+console.log(get_ip_addr())
 app.listen(PORT, () => {
 	console.log(`Example app listening at http://localhost:${PORT}`);
 });
