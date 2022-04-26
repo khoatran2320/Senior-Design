1640a15df489bcea5d08d52f13ca7e011362e250
kenchan423 <55323049+kenchan423@users.noreply.github.com>
2022-04-25 22:17:43 -0400

    Update beeper.py

--- boxi/server/beeper.py
+++ boxi/server/beeper.py
@@ -11,20 +11,20 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(beeper_trip, GPIO.OUT)
 
 # trip func: outputs on gpio pin 36 for 1 millisecond
-def beep(iterations=20):
-	buzzer = GPIO.PWM(beeper_trip,1000)
+def beep():
+	#buzzer = GPIO.PWM(beeper_trip,1000)
 	n = 0
-	while(n < iterations):
+	while(n < 1):
 		#buzzer = GPIO.PWM(beeper_trip, 1000)
 		print("beeping")
-		buzzer.start(10)
-		#GPIO.output(beeper_trip, GPIO.HIGH)
+		#buzzer.start(10)
+		GPIO.output(beeper_trip, GPIO.HIGH)
 		time.sleep(0.5)
 		print("beeped")
 		buzzer.stop()
 		time.sleep(0.5)
 		n += 1
-		#GPIO.output(beeper_trip, GPIO.LOW) 
+		GPIO.output(beeper_trip, GPIO.LOW) 
 		#time.sleep(1)
 
 if __name__ == "__main__":
