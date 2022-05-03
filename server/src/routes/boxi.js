3e8ff96cb38db54a36d7915bc7efeabed3a8c498
yanchen01 <ychen01@bu.edu>
2022-05-03 11:30:27 -0400

    added boxi alarm enable routes

--- server/src/routes/boxi.js
+++ server/src/routes/boxi.js
@@ -455,6 +455,129 @@ router.get('/alarm-status', async (req, res) => {
 	return;
 });
 
+router.post('/alarm-enable', async (req, res) => {
+	// Turning on/off alarm on a Boxi given Boxi ID
+	const { userId, boxiId, alarmEnable } = req.body;
+
+	if (!userId) {
+		res.status(400).send('Requires a user ID!');
+		return;
+	}
+	if (!boxiId) {
+		res.status(400).send('Requires a box ID!');
+		return;
+	}
+
+	// check if user exist
+	try {
+		await auth.getAuth().getUser(userId);
+	} catch (err) {
+		//unable to find user
+		if (err['errorInfo']['code'] == 'auth/user-not-found') {
+			res.status(400).send('User does not exist');
+			return;
+		}
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+
+	// check if user owns this box
+	let userOwnsBox = false;
+	try {
+		const sshot = await db.collection(PACKAGE_COLLECTION).doc(userId).collection('boxes').get();
+		sshot.forEach((doc) => {
+			if (doc.id == boxiId) {
+				userOwnsBox = true;
+			}
+		});
+	} catch (err) {
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+	if (!userOwnsBox) {
+		res.status(403).send('User does not have access to this box!');
+		return;
+	}
+
+	// check if boxiId exist
+	const boxiRef = db.collection(BOXI_COLLECTION).doc(boxiId);
+	const boxiDoc = await boxiRef.get();
+
+	if (!boxiDoc.exists) {
+		res.status(400).send('Invalid Boxi ID');
+		return;
+	}
+
+	// update the boxi status to toggle alarm
+	try {
+		await boxiRef.update({ alarm_enable: alarmEnable });
+	} catch (err) {
+		res.status(400).send('Failed to post boxi alarm enable');
+		return;
+	}
+
+	res.status(200).send('Success!');
+	return;
+});
+
+router.get('/alarm-enable', async (req, res) => {
+	// Get Boxi unlock status: true, false
+	const { userId, boxiId } = req.query;
+
+	if (!userId) {
+		res.status(400).send('Requires a user ID!');
+		return;
+	}
+	if (!boxiId) {
+		res.status(400).send('Requires a box ID!');
+		return;
+	}
+
+	// check if user exist
+	try {
+		await auth.getAuth().getUser(userId);
+	} catch (err) {
+		//unable to find user
+		if (err['errorInfo']['code'] == 'auth/user-not-found') {
+			res.status(400).send('User does not exist');
+			return;
+		}
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+
+	// check if user owns this box
+	let userOwnsBox = false;
+	try {
+		const sshot = await db.collection(PACKAGE_COLLECTION).doc(userId).collection('boxes').get();
+		sshot.forEach((doc) => {
+			if (doc.id == boxiId) {
+				userOwnsBox = true;
+			}
+		});
+	} catch (err) {
+		res.status(400).send('Something went wrong!');
+		return;
+	}
+	if (!userOwnsBox) {
+		res.status(403).send('User does not have access to this box!');
+		return;
+	}
+
+	// check if boxiId exist
+	const boxiRef = db.collection(BOXI_COLLECTION).doc(boxiId);
+	const boxiDoc = await boxiRef.get();
+
+	if (!boxiDoc.exists) {
+		res.status(400).send('Invalid Boxi ID');
+		return;
+	}
+
+	const alarmEnable = boxiDoc.data()['alarm_enable'];
+	res.status(200).send({ status_code: 200, data: alarmEnable, msg: 'Success!' });
+	return;
+});
+
 router.post('/post-ip', async (req, res) => {
 	// Turning on/off alarm on a Boxi given Boxi ID
 	const { userId, boxiId, ipAddr, port} = req.body;
@@ -520,5 +643,7 @@ router.post('/post-ip', async (req, res) => {
 	res.status(200).send('Success!');
 	return;
 });
+
+
 
 module.exports = router;
